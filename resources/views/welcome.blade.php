2c10fb63e234aea16a780cd85ce4c766fa2a2df9
seawboon <seawboon.yeap@gmail.com>
2019-12-31 16:56:19 +0800

    wth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,10 @@
 <div class="container" style="height: auto;">
   <div class="row justify-content-center">
       <div class="col-lg-7 col-md-8">
-          <h1 class="text-white text-center">{{ __('Welcome to Material Dashboard FREE Laravel sds ss Live Preview. hohoho') }}</h1>
+          <h1 class="text-white text-center">{{ __('Welcome to Material Dashboard FREE Laravel sds ss Live Preview.') }}</h1>
+          <p>
+            wth
+          </p>
       </div>
   </div>
 </div>
