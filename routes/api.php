5471be20cf9ec9ea6a5290ac713e0d9d4af67c1b
seawboon <seawboon.yeap@gmail.com>
2019-12-31 12:31:51 +0800

    updates

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/github-hooks', function(){
-  Artisan::call('git:deploy')
+  Artisan::call('git:deploy');
+  exit;
 });
